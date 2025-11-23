c167615c5f0e2131a732a40afb8ae4ab67dc8787
itsJefryTz <thejefryurdaneta21@gmail.com>
2025-11-23 13:27:26 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import { useEffect } from 'react';
+
+function App() {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch('https://codes.itsjefryservices.app', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+        
+        if (!res.ok) {
+          throw new Error(`HTTP error! status: ${res.status}`);
+        }
+        
+        const data = await res.json();
+        console.log('Response:', data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
